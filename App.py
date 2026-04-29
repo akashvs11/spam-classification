d48c076c73bb3a200a761418c3c5ce68b8a318a4
akashvs11 <akashvs8276@gmail.com>
2026-04-29 21:20:39 +0530

    first commit

---
+++ App.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, session, flash, url_for
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, EqualTo
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import train_test_split
+import os
+import pandas as pd
+
+app = Flask(__name__, template_folder='templates')
+app.static_folder = 'static'
+app.secret_key = 'your_secret_key'
+
+@app.route('/')
+def home():
+    return render_template('home.html', title="Home")
+
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    df = pd.read_csv("spam (1).csv", encoding="latin-1")
+    df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+    # Features and Labels
+
+    df['label'] = df['class'].map({'ham': 0, 'spam': 1})
+    X = df['message']
+    y = df['label']
+    # Extract Feature With CountVectorizer
+    cv = CountVectorizer()
+    X = cv.fit_transform(X)  # Fit the Data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
+    # Naive Bayes Classifier
+    clf = MultinomialNB()
+    clf.fit(X_train, y_train)
+    clf.score(X_test, y_test)
+    if request.method == 'POST':
+        message = request.form['message']
+        data = [message]
+        vect = cv.transform(data).toarray()
+        my_prediction = clf.predict(vect)
+        return render_template('index.html', prediction=my_prediction)
+
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
